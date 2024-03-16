f77a32543bd7f49fccba3fd60e21b73c2f1a6706
Dagmawi Yohannes <dagmawiyohannes323@gmail.com>
2024-03-16 20:11:05 +0300

    Last touch

--- main.py
+++ main.py
@@ -2,3 +2,11 @@ import tkinter as tk
 from TicTacToeGUI import GUI
 # from TicTacToeGame import Game
 # from tkinter import messagebox
+
+def main ():
+    root = tk.Tk()
+    game = GUI(root)
+    root.mainloop()
+    
+if __name__ == "__main__":
+    main()
